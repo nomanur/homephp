e2651d7aca05bcafb5e59678a21e80b8054df873
Nomanur Rahman <nomanurrahman@gmail.com>
2022-03-05 10:37:26 +0600

    first class

---
+++ index.php
@@ -0,0 +1,114 @@
+<?php
+
+// echo 'I am learning php';
+
+// print('i am learning php');
+
+// printf('I am learning %s %s', 'php', 'second');
+
+// var_dump('hello');
+
+// string = "any text";
+// number = 1 ,2,3;
+// boolean = true, false
+// $x = 5;
+
+// if ('http://api.com') {
+// }else{
+//     echo;
+// }
+
+
+// try {
+//     if ($x == 6) {
+//         echo $x;
+//     } else {
+//         throw new Error('Condition not true');
+//     }
+// } catch (\Throwable $th) {
+//     echo $th->getMessage();
+// }
+
+$array = [2 => 1, 'one' => 2, 3, 4];
+
+// var_dump($array['one']);
+// echo $array[3];
+
+// foreach ($array as $a) {
+//     echo $a;
+// }
+
+
+// $x = 'I am learning';
+
+// $y = 'PHP';
+
+// echo $x . '&nbsp' .  $y;
+
+// var_dump($_POST);
+
+// $x = 5;
+
+// $x = 6;
+
+// const y = 8;
+// const z = 9;
+// echo y;
+?>
+
+<!-- prob -->
+<!-- find all the even number from 0 to 10 using if elseif and else; -->
+<!-- take value from radio button to php -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <!-- <form action="" method="post">
+        <input type="text" name="name">
+        <input type="text" name="email">
+        <button type="submit">Submit</button>
+    </form> -->
+
+    <!-- <style>
+        .full-wrapper {
+            width: 100%;
+        }
+
+        .wrapper {
+            width: 1170px;
+            margin: 0 auto;
+        }
+
+        .bg {
+            background: #1f1b24;
+            opacity: .5;
+        }
+
+        h2 {
+            color: #fff;
+        }
+    </style> -->
+
+    <!-- <div class="full-wrapper">
+        <div class="wrapper">
+            foreach{
+            <div class="bg">
+                <h2>Kubb</h2>
+            </div>
+            }
+        </div>
+    </div> -->
+
+
+</body>
+
+</html>
